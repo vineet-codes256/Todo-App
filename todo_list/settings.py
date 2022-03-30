b9ea4212f898f76dfe2e9b61d719a9240b043f3d
Vineet Rawat <vineetrawat256@gmail.com>
2022-03-30 06:34:42 +0530

    updated settings.py(debug-off)

--- todo_list/settings.py
+++ todo_list/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-yae9+#pi4nz5*vxnlyvpf23^058eiy04qxee7hwhw5!=ke_8(h'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["https://tasks-todo-app.herokuapp.com/", '127.0.0.1']
 
 
 # Application definition
