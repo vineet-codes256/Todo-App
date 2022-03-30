b27cce380df3aab1e51f2bb8d081d7970b2790e4
Vineet Rawat <vineetrawat256@gmail.com>
2022-03-30 18:21:47 +0530

    Included favicon

--- base/urls.py
+++ base/urls.py
@@ -1,11 +1,15 @@
-from django.urls import URLPattern, path
+from django.urls import URLPattern, path, include
 from .views import TaskList, TaskDetail, TaskCreate, TaskUpdate, TaskDelete, CustomLoginView, RegisterPage
 from django.contrib.auth.views import LogoutView
+from django.contrib.staticfiles.storage import staticfiles_storage
+from django.views.generic.base import RedirectView
+from django.conf import settings
 
 urlpatterns = [
     path('login/', CustomLoginView.as_view(), name='login'),
     path('logout/', LogoutView.as_view(next_page='login'), name='logout'),
     path('register/', RegisterPage.as_view(), name='register'),
+    path('favicon.ico', RedirectView.as_view(url=staticfiles_storage.url("favicon.ico"))),
 
     path('', TaskList.as_view(), name='tasks'),
     path('task/<int:pk>/', TaskDetail.as_view(), name='task'),
